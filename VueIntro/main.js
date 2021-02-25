58af0f437c83722209cd1cf9191a40cd6001da3d
Nakagochi <christensentysen@gmail.com>
2021-02-24 22:01:20 -0700

    This is the intro to Vue

---
+++ VueIntro/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Boots",
+      desc: "These are things you put on your feeeeet!",
+      img: "./assets/images/socks_green.jpg",
+      url: "http://wp.tysenchristensen.tech/",
+    };
+  },
+});
